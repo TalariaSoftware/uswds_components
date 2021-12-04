a8934ea61aab97134eb34ec28665f100f3815013
Damien Burke <damien@damienburke.com>
2021-12-04 10:58:00 -0800

    Add alert component

---
+++ app/components/uswds_components/alert_component.rb
@@ -0,0 +1,12 @@
+module UswdsComponents
+  class AlertComponent < ViewComponent::Base
+    attr_accessor :status, :include_icon, :heading
+
+    def initialize(status: 'info', heading: '', include_icon: true)
+      super
+      @status = status.presence || 'info'
+      @include_icon = include_icon
+      @heading = heading
+    end
+  end
+end
