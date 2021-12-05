96e5ae06a1fff523e67e87d61162cc5172737bc4
Damien Burke <damien@damienburke.com>
2021-12-05 15:04:26 -0800

    Add friendly layout to component previews

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -22,6 +22,7 @@ module Dummy
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.view_component.default_preview_layout = 'application'
     config.view_component.preview_paths <<
       UswdsComponents::Engine.root.join('spec/components/previews')
   end
