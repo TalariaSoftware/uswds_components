1cca70c8f1dcc2454974b262ed51dac104b352a8
Damien Burke <damien@damienburke.com>
2021-12-17 21:01:04 -0800

    Add Basic Header Component

---
+++ app/components/uswds_components/basic_header_component.rb
@@ -0,0 +1,58 @@
+module UswdsComponents
+  class BasicHeaderComponent < ViewComponent::Base
+    attr_accessor :image_path
+
+    renders_one :logo, 'BasicLogo'
+    renders_many :primary_items, 'PrimaryItem'
+
+    def initialize(image_path: '/assets/img')
+      super
+      @image_path = image_path
+    end
+
+    def close_image
+      "#{image_path}/usa-icons/close.svg"
+    end
+
+    class BasicLogo < ViewComponent::Base
+      attr_accessor :title, :href
+
+      def initialize(title: '', href: '/')
+        super
+        @title = title
+        @href = href
+      end
+    end
+
+    class PrimaryItem < ViewComponent::Base
+      attr_accessor :title, :href
+
+      renders_many :submenu_items, 'UswdsComponents::BasicHeaderComponent::SubmenuItem'
+
+      def initialize(title: '', href: '/', current: false)
+        super
+        @title = title
+        @href = href
+        @current = current
+      end
+
+      def submenu_id
+        @submenu_id ||= "usa-nav__submenu_#{object_id.to_s(36)}"
+      end
+
+      def current
+        @current || current_page?(href)
+      end
+    end
+
+    class SubmenuItem < ViewComponent::Base
+      attr_accessor :name, :href
+
+      def initialize(name: '', href: '/')
+        super
+        @name = name
+        @href = href
+      end
+    end
+  end
+end
---
+++ spec/components/previews/basic_header_component_preview.rb
@@ -0,0 +1,43 @@
+class BasicHeaderComponentPreview < ViewComponent::Preview
+  def default
+    render UswdsComponents::BasicHeaderComponent.new(
+      image_path: 'https://cdn.jsdelivr.net/npm/uswds@2.13.0/dist/img',
+    ) do |header|
+
+      header.logo(href: '#', title: 'Project Title')
+
+      header.primary_item(title: 'Current Section', current: true) do |item|
+        item.submenu_item(href: '', name: 'Navigation link')
+        item.submenu_item(href: '', name: 'Navigation link')
+        item.submenu_item(href: '', name: 'Navigation link')
+      end
+
+      header.primary_item(title: 'Section') do |item|
+        item.submenu_item(href: '', name: 'Navigation link')
+        item.submenu_item(href: '', name: 'Navigation link')
+        item.submenu_item(href: '', name: 'Navigation link')
+      end
+
+      header.primary_item(href: '#', title: 'Simple link')
+
+      raw('<form class="usa-search usa-search--small" role="search">
+        <label class="usa-sr-only" for="basic-search-field-en-small">
+          Search
+        </label>
+        <input
+          class="usa-input"
+          id="basic-search-field-en-small"
+          type="search"
+          name="search"
+        />
+        <button class="usa-button" type="submit">
+          <img
+            src="https://cdn.jsdelivr.net/npm/uswds@2.13.0/dist/img/usa-icons-bg/search--white.svg"
+            class="usa-search__submit-icon"
+            alt="Search"
+          />
+        </button>
+      </form>')
+    end
+  end
+end
