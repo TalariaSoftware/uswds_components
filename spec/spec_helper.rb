c6c36e87c5fc96c787e2bbb786f6c6ee3fa55105
Damien Burke <damien@damienburke.com>
2022-03-27 12:05:40 -0700

    Use single quotes for string

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.filter_run_when_matching :focus
-  config.example_status_persistence_file_path = "tmp/examples.txt"
+  config.example_status_persistence_file_path = 'tmp/examples.txt'
   config.disable_monkey_patching!
 
   # if config.files_to_run.one?
