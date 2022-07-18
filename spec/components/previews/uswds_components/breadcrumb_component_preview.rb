9466f0ad9046451a6f62c4d4ce109b0860ce8de8
Damien Burke <damien@damienburke.com>
2022-07-17 23:33:30 -0700

    Parameterize breadcrumb component previews

--- spec/components/previews/uswds_components/breadcrumb_component_preview.rb
+++ spec/components/previews/uswds_components/breadcrumb_component_preview.rb
@@ -1,35 +1,35 @@
 class UswdsComponents::BreadcrumbComponentPreview < ViewComponent::Preview
-  def default
-    render UswdsComponents::BreadcrumbComponent.new do |trail|
+  # Breadcrumb Component
+  # --------------------
+  #
+  # [See at USWDS](https://designsystem.digital.gov/components/breadcrumb/)
+  #
+  # @param wrap toggle
+  def default(wrap: false)
+    render UswdsComponents::BreadcrumbComponent.new(wrap: wrap) do |trail|
       trail.crumb(href: '#home', name: 'Home')
       trail.crumb(href: '#contracting', name: 'Federal Contracting')
       trail.crumb(href: '#assistance', name: 'Contracting assistance programs')
       trail.crumb(
-        href: '/rails/view_components/uswds_components/breadcrumb_component/default',
+        href: '/lookbook/preview/uswds_components/breadcrumb/default',
         name: 'Women-owned small business federal contracting program',
       )
     end
   end
 
-  def wrapping
-    render UswdsComponents::BreadcrumbComponent.new(wrap: true) do |trail|
-      trail.crumb(href: '#home', name: 'Home')
-      trail.crumb(href: '#contracting', name: 'Federal Contracting')
-      trail.crumb(href: '#assistance', name: 'Contracting assistance programs')
-      trail.crumb(
-        href: '/rails/view_components/uswds_components/breadcrumb_component/wrapping',
-        name: 'Women-owned small business federal contracting program',
-      )
-    end
-  end
-
-  def blank_paths
-    render UswdsComponents::BreadcrumbComponent.new do |trail|
+  # Breadcrumb Component
+  # --------------------
+  #
+  # [See at USWDS](https://designsystem.digital.gov/components/breadcrumb/)
+  #
+  # @param wrap toggle
+  def blank_paths(wrap: false)
+    render UswdsComponents::BreadcrumbComponent.new(wrap: wrap) do |trail|
       trail.crumb(href: '#home', name: 'Home')
       trail.crumb(href: '', name: 'Federal Contracting')
       trail.crumb(href: '', name: 'Contracting assistance programs')
       trail.crumb(
-        href: '/rails/view_components/uswds_components/breadcrumb_component/blank_paths',
+        href: '/lookbook/preview/uswds_components/breadcrumb/blank_paths',
         name: 'Women-owned small business federal contracting program',
       )
     end
