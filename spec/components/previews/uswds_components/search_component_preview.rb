46e5aed6bb9b5ffd0e76980fbbb34d51e421fdaf
Damien Burke <damien@damienburke.com>
2022-07-17 23:05:30 -0700

    Parameterize search component preview

--- spec/components/previews/uswds_components/search_component_preview.rb
+++ spec/components/previews/uswds_components/search_component_preview.rb
@@ -1,22 +1,13 @@
 class UswdsComponents::SearchComponentPreview < ViewComponent::Preview
-  def default
+  # Search Component
+  # ----------------
+  #
+  # [See at USWDS](https://designsystem.digital.gov/components/search/)
+  #
+  # @param size [Symbol] select [[Default, nil], [Small, small], [Big, big]]
+  def default(size: nil)
     render UswdsComponents::SearchComponent.new(
-      form_options: { url: '#' },
-      submit_icon: 'https://cdn.jsdelivr.net/npm/uswds@2.13.0/src/img/usa-icons-bg/search--white.svg',
-    )
-  end
-
-  def big
-    render UswdsComponents::SearchComponent.new(
-      size: :big,
-      form_options: { url: '#' },
-      submit_icon: 'https://cdn.jsdelivr.net/npm/uswds@2.13.0/src/img/usa-icons-bg/search--white.svg',
-    )
-  end
-
-  def small
-    render UswdsComponents::SearchComponent.new(
-      size: :small,
+      size: size,
       form_options: { url: '#' },
       submit_icon: 'https://cdn.jsdelivr.net/npm/uswds@2.13.0/src/img/usa-icons-bg/search--white.svg',
     )
